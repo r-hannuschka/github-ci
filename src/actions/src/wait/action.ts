f320ac5f71b2dbd65af9b5344a0508b6bb49f1dd
Ralf Hannuschka <r.hannuschka@gmail.com>
2019-11-07 18:01:19 +0100

    add base angular app

--- src/actions/src/wait/action.ts
+++
@@ -1,8 +0,0 @@
-export function wait(milliseconds: number): Promise<string> {
-    return new Promise((resolve) => {
-        if (isNaN(milliseconds)) { 
-            throw new Error('milleseconds not a number'); 
-        }
-        setTimeout(() => resolve(), milliseconds)
-    });
-}
