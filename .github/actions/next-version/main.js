3c19c0561a74209af9d7f70146a96036a484fd37
Ralf Hannuschka <r.hannuschka@gmail.com>
2019-11-10 20:42:21 +0100

    display next version in workflow

--- .github/actions/next-version/main.js
+++ .github/actions/next-version/main.js
@@ -27,8 +27,7 @@ let gitLog = null;
 /** get git log since last tag */
 try {
     lastTag = child_process_1.execSync(`git describe --tags --abbrev=0`).toString();
-    lastTag = lastTag.replace(/(^\s*|\s$)/g, "");
-    console.log(lastTag.length);
+    lastTag = lastTag.replace(/(^\s*|\s*$)/g, "");
     gitLog = child_process_1.execSync(`git log ${lastTag}..HEAD --pretty=oneline`).toString();
 }
 catch (error) {
